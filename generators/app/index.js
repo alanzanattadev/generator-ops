f33fad078f56554060ad18a3ef3e2425c3f0c86d
alanzanatta <alan.zanatta@epitech.eu>
2014-11-05 14:14:52 +0100

    restructured prompts

--- generators/app/index.js
+++ generators/app/index.js
@@ -10,90 +10,104 @@ module.exports = yeoman.generators.Base.extend({
   constructor: function() {
     yeoman.generators.Base.apply(this, arguments);
   },
-  initHierarchy: function() {
-    this.sourceRoot(this.sourceRoot() + '/../../../templates');
-    this.dest.mkdir('www', '755');
-    this.dest.mkdir('dist', '755');
-    this.dest.mkdir('www/css', '755');
-    this.dest.mkdir('www/img', '755');
-    this.dest.mkdir('www/js', '755');
-    this.dest.mkdir('tests', '755');
-    this.dest.mkdir('tests/unit', '755');
-    this.dest.mkdir('tests/e2e', '755');
+  initializing: {
+    initHierarchy: function() {
+      this.sourceRoot(this.sourceRoot() + '/../../../templates');
+      this.dest.mkdir('www', '755');
+      this.dest.mkdir('dist', '755');
+      this.dest.mkdir('www/css', '755');
+      this.dest.mkdir('www/img', '755');
+      this.dest.mkdir('www/js', '755');
+      this.dest.mkdir('tests', '755');
+      this.dest.mkdir('tests/unit', '755');
+      this.dest.mkdir('tests/e2e', '755');
+    },
+    initConf: function() {
+      this.src.copy('.gitignore', '.gitignore');
+      this.src.copy('.bowerrc', '.bowerrc');
+    }
   },
-  promptInfos: function() {
-    var done = this.async();
-    this.prompt([{
-      type    : 'input',
-      name    : 'name',
-      message : 'Project Name',
-      default : this.appname // Default to current folder name
-    }, {
-      type    : 'input',
-      name    : 'desc',
-      message : 'Project Desc',
-      default : this.appname // Default to current folder name
-    }, {
-      type    : 'input',
-      name    : 'author',
-      message : 'Project Author',
-      default : this.appname // Default to current folder name
-    }], function (answers) {
-      var packagejson = {
-        version: '0.1.0',
-        main: 'index.js'
-      };
-      packagejson.name = answers.name;
-      packagejson.description = answers.desc;
-      packagejson.author = answers.author;
-      this.dest.write("package.json", JSON.stringify(packagejson, undefined, 4));
-      this.dest.write("bower.json", JSON.stringify(packagejson, undefined, 4));
-      done();
-    }.bind(this));
-  },
-  writeConf: function() {
-    this.src.copy('.gitignore', '.gitignore');
-    this.src.copy('.bowerrc', '.bowerrc');
-  },
-  promptFrameworks: function() {
-    var done = this.async();
-    this.prompt([{
-      type    : 'checkbox',
-      name    : 'frameworks',
-      choices : ['angularjs', 'polymer', 'Unit Tests Framework'],
-      message : 'Choose frameworks you want to use',
-      default : []
-    }, {
-      when    : function(answer) {
-        return answer.frameworks.contain('angularjs');
-      },
-      type    : 'checkbox',
-      name    : 'angularmods',
-      choices : ['angular-route', 'angular-resources', 'angular-touch', 'angular-mocks'],
-      message : 'Choose angular modules you want to use',
-      default : []
-    }, {
-      when    : function(answer) {
-        return answer.frameworks.contain('Unit Tests Framework');
-      },
-      type    : 'list',
-      name    : 'unittestframework',
-      choices : ['jasmine', 'mocha', 'qunitjs'],
-      message : 'Choose which unit test framework you want to use',
-      default : 'jasmine'
-    }], function (answers) {
-      for (i in answers.frameworks)
-        {
-          if (answers.frameworks[i] != 'Unit Tests Framework')
-            {
-              this.bowerInstall(answers.frameworks[i], {'save':true}, done);
-            }
-        }
-      if (answers.frameworks.contain('Unit Tests Framework'))
-        {
-          this.npmInstall(answers.unittestframework, {'save':true}, done);
-        }
-      done();
-    }.bind(this));
+  prompting: {
+    promptInfos: function() {
+      var done = this.async();
+      this.prompt([{
+        type    : 'input',
+        name    : 'name',
+        message : 'Project Name',
+        default : this.appname // Default to current folder name
+      }, {
+        type    : 'input',
+        name    : 'desc',
+        message : 'Project Desc',
+        default : this.appname // Default to current folder name
+      }, {
+        type    : 'input',
+        name    : 'author',
+        message : 'Project Author',
+        default : this.appname // Default to current folder name
+      }], function (answers) {
+        var packagejson = {
+          version: '0.1.0',
+          main: 'index.js'
+        };
+        packagejson.name = answers.name;
+        packagejson.description = answers.desc;
+        packagejson.author = answers.author;
+        this.dest.write("package.json", JSON.stringify(packagejson, undefined, 4));
+        this.dest.write("bower.json", JSON.stringify(packagejson, undefined, 4));
+        done();
+      }.bind(this));
+    },
+    promptFrameworks: function() {
+      var done = this.async();
+      this.prompt([{
+        type    : 'checkbox',
+        name    : 'frameworks',
+        choices : ['angularjs', 'polymer', 'Unit Tests Framework'],
+        message : 'Choose frameworks you want to use',
+        default : []
+      }, {
+        when    : function(answer) {
+          return answer.frameworks.contain('angularjs');
+        },
+        type    : 'checkbox',
+        name    : 'angularmods',
+        choices : ['angular-route', 'angular-resources', 'angular-touch', 'angular-mocks'],
+        message : 'Choose angular modules you want to use',
+        default : []
+      }, {
+        when    : function(answer) {
+          return answer.frameworks.contain('Unit Tests Framework');
+        },
+        type    : 'list',
+        name    : 'unittestframework',
+        choices : ['jasmine', 'mocha', 'qunitjs'],
+        message : 'Choose which unit test framework you want to use',
+        default : 'jasmine'
+      }], function (answers) {
+        for (i in answers.frameworks)
+          {
+            this.log('...salut...');
+            if (answers.frameworks[i] != 'Unit Tests Framework')
+              {
+                this.log('...ok...');
+                this.bowerInstall([answers.frameworks[i]], {'save':true}, done);
+                this.log('...good...');
+              }
+          }
+        if (answers.frameworks.contain('Unit Tests Framework'))
+          {
+            this.npmInstall([answers.unittestframework], {'save':true}, done);
+          }
+        if (answers.frameworks.contain('angularjs'))
+          {
+            for (i in answers.angularmods)
+              {
+                this.bowerInstall([answers.angularmods[i]], {'save':true}, done);
+              }
+          }
+        done();
+      }.bind(this));
+    }
   }
 });
