11930f86b90af29b5b9a1ab0d02451f2cf884649
alanzanatta <alan.zanatta@epitech.eu>
2014-11-05 16:36:48 +0100

    Added many questions, grouped categories

--- generators/app/index.js
+++ generators/app/index.js
@@ -15,9 +15,15 @@ module.exports = yeoman.generators.Base.extend({
       this.sourceRoot(this.sourceRoot() + '/../../../templates');
       this.dest.mkdir('www', '755');
       this.dest.mkdir('dist', '755');
-      this.dest.mkdir('www/css', '755');
-      this.dest.mkdir('www/img', '755');
+      this.dest.mkdir('config', '755');
+      this.dest.mkdir('config/production', '755');
+      this.dest.mkdir('config/development', '755');
+      this.dest.mkdir('config/all', '755');
       this.dest.mkdir('www/js', '755');
+      this.dest.mkdir('www/views', '755');
+      this.dest.mkdir('www/assets', '755');
+      this.dest.mkdir('www/css', '755');
+      this.dest.mkdir('www/assets/img', '755');
       this.dest.mkdir('tests', '755');
       this.dest.mkdir('tests/unit', '755');
       this.dest.mkdir('tests/e2e', '755');
@@ -62,22 +68,13 @@ module.exports = yeoman.generators.Base.extend({
       var done = this.async();
       this.prompt([{
         type    : 'checkbox',
-        name    : 'frameworks',
-        choices : ['angularjs', 'polymer', 'Unit Tests Framework'],
-        message : 'Choose frameworks you want to use',
+        name    : 'frameworkstype',
+        choices : ['Javascript Structure Frameworks', 'WebComponent Frameworks', 'Unit Tests Framework'],
+        message : 'Choose frameworks types you want to use',
         default : []
       }, {
         when    : function(answer) {
-          return answer.frameworks.contain('angularjs');
-        },
-        type    : 'checkbox',
-        name    : 'angularmods',
-        choices : ['angular-route', 'angular-resource', 'angular-touch', 'angular-mocks'],
-        message : 'Choose angular modules you want to use',
-        default : []
-      }, {
-        when    : function(answer) {
-          return answer.frameworks.contain('Unit Tests Framework');
+          return answer.frameworkstype.contain('Unit Tests Framework');
         },
         type    : 'list',
         name    : 'unittestframework',
@@ -87,23 +84,100 @@ module.exports = yeoman.generators.Base.extend({
                   ],
         message : 'Choose which unit test framework you want to use',
         default : 'jasmine'
+      }, {
+        when    : function(answer) {
+          return answer.frameworkstype.contain('Javascript Structure Frameworks');
+        },
+        type    : 'checkbox',
+        name    : 'structureframeworks',
+        choices : [{'name':'AngularJS', 'value':'angularjs'},
+                    {'name':'EmberJS', 'value':'ember'},
+                    {'name':'BackboneJS', 'value':'backbone'},
+                    {'name':'RequireJS', 'value':'requirejs'},
+                    {'name':'JQuery', 'value':'jquery'}
+                  ],
+        message : 'Choose structure frameworks you want to use',
+        default : []
+      }, {
+        when    : function(answer) {
+          return answer.structureframeworks.contain('angularjs');
+        },
+        type    : 'checkbox',
+        name    : 'angularmods',
+        choices : ['angular-route', 'angular-resource', 'angular-touch', 'angular-mocks', 'angular-animate'],
+        message : 'Choose angular modules you want to use',
+        default : []
+      }, {
+        when    : function(answer) {
+          return answer.frameworkstype.contain('WebComponent Frameworks');
+        },
+        type    : 'checkbox',
+        name    : 'webcomponentframeworks',
+        choices : ["Polymer",
+                    "X-Tag",
+                    {'name':'Bootstrap', 'value':'bootstrap'},
+                    {'name':'Foundation', 'value':'foundation'},
+                    {'name':'JQueryUI', 'value':'jquery-ui'},
+                    {'name':'JQueryMobile', 'value':'jquery-mobile'}
+                  ],
+        message : 'Choose structure frameworks you want to use',
+        default : []
+      }, {
+        when    : function(answer) {
+          return answer.webcomponentframeworks.contain('Polymer');
+        },
+        type    : 'checkbox',
+        name    : 'polymermods',
+        choices : [
+                    {'name':'Polymer Core Elements', 'value':'Polymer/core-elements'},
+                    {'name':'Polymer Paper Elements', 'value':'Polymer/paper-elements'}
+                  ],
+        message : 'Choose polymer mods you want to use',
+        default : []
       }], function (answers) {
-        for (i in answers.frameworks)
+        if (answers.frameworkstype.contain('WebComponent Frameworks'))
           {
-            if (answers.frameworks[i] != 'Unit Tests Framework')
+            for (i in answers.webcomponentframeworks)
               {
-                this.bowerInstall([answers.frameworks[i]], {'save':true}, done);
+                switch (answers.webcomponentframeworks[i])
+                {
+                  case "Polymer":
+                    this.bowerInstall(["Polymer/polymer"], {'save':true}, done);
+                    for (j in answers.polymermods)
+                      {
+                        this.bowerInstall([answers.polymermods[j]], {'save':true}, done);
+                      }
+                    break;
+                  case "X-Tag":
+                    // A Completer
+                    break;
+                  default:
+                    this.bowerInstall([answers.webcomponentframeworks[i]], {'save':true}, done);
+                }
               }
           }
-        if (answers.frameworks.contain('Unit Tests Framework'))
+        if (answers.frameworkstype.contain('Unit Tests Framework'))
           {
             this.npmInstall([answers.unittestframework], {'save':true}, done);
           }
-        if (answers.frameworks.contain('angularjs'))
+        if (answers.frameworkstype.contain('Javascript Structure Frameworks'))
           {
-            for (i in answers.angularmods)
+            for (i in answers.structureframeworks)
               {
-                this.bowerInstall([answers.angularmods[i]], {'save':true}, done);
+                switch (answers.structureframeworks[i])
+                {
+                  case "angularjs":
+                    for (i in answers.angularmods)
+                      {
+                        this.bowerInstall([answers.angularmods[i]], {'save':true}, done);
+                      }
+                    this.dest.mkdir('www/js/controllers', '755');
+                    this.dest.mkdir('www/js/services', '755');
+                    this.dest.mkdir('www/js/routes', '755');
+                    break;
+                  default:
+                    this.bowerInstall([answers.structureframeworks[i]], {'save':true}, done);
+                }
               }
           }
         done();
