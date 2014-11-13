8efb5587c2db9f22918aa93fbec4ac769f27dd7c
alanzanatta <alan.zanatta@epitech.eu>
2014-11-13 19:24:22 +0100

    fixed error in install from base stack

--- generators/app/install.js
+++ generators/app/install.js
@@ -1,44 +1,47 @@
 exports.install = function(answers, that, done) {
-  if (answers.frameworkstype.contain('WebComponent Frameworks'))
+  if (answers.frameworkstype != undefined)
     {
-      for (i in answers.webcomponentframeworks)
-        {
-          switch (answers.webcomponentframeworks[i])
+        if (answers.frameworkstype.contain('WebComponent Frameworks'))
           {
-            case "Polymer/polymer":
-              that.bowerInstall([answers.webcomponentframeworks[i]], {'save':true}, done);
-              for (j in answers.polymermods)
+            for (i in answers.webcomponentframeworks)
+              {
+                switch (answers.webcomponentframeworks[i])
                 {
-                  that.bowerInstall([answers.polymermods[j]], {'save':true}, done);
+                  case "Polymer/polymer":
+                    that.bowerInstall([answers.webcomponentframeworks[i]], {'save':true}, done);
+                    for (j in answers.polymermods)
+                      {
+                        that.bowerInstall([answers.polymermods[j]], {'save':true}, done);
+                      }
+                    break;
+                  case "X-Tag":
+                    // A Completer
+                    break;
+                  default:
+                    that.bowerInstall([answers.webcomponentframeworks[i]], {'save':true}, done);
                 }
-              break;
-            case "X-Tag":
-              // A Completer
-              break;
-            default:
-              that.bowerInstall([answers.webcomponentframeworks[i]], {'save':true}, done);
+              }
           }
-        }
-    }
-  if (answers.frameworkstype.contain('Unit Tests Framework'))
-    {
-      that.npmInstall([answers.unittestframework], {'save':true}, done);
-    }
-  if (answers.frameworkstype.contain('Javascript Structure Frameworks'))
-    {
-      for (i in answers.structureframeworks)
-        {
-          switch (answers.structureframeworks[i])
+        if (answers.frameworkstype.contain('Unit Tests Framework'))
           {
-            case "angularjs":
-              for (i in answers.angularmods)
-                {
-                  that.bowerInstall([answers.angularmods[i]], {'save':true}, done);
-                }
-            default:
-              that.bowerInstall([answers.structureframeworks[i]], {'save':true}, done);
+            that.npmInstall([answers.unittestframework], {'save':true}, done);
           }
-        }
+        if (answers.frameworkstype.contain('Javascript Structure Frameworks'))
+          {
+            for (i in answers.structureframeworks)
+              {
+                switch (answers.structureframeworks[i])
+                {
+                  case "angularjs":
+                    for (i in answers.angularmods)
+                      {
+                        that.bowerInstall([answers.angularmods[i]], {'save':true}, done);
+                      }
+                  default:
+                    that.bowerInstall([answers.structureframeworks[i]], {'save':true}, done);
+                }
+              }
+          }
     }
 };
 
